c1e1ec3980d96982552744ca5b804e9382ff9273
Poonam <poonamendole242@gmail.com>
2022-06-22 22:01:50 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  const name = "Sudarshan070";
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${name}`)
+      .then((res) => setData(res.data));
+  }, []);
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="one">
+      <div className="two">
+        <img src={data && data.avatar_url} alt="avatar" />
+        <div className="three">
+          <h2> {data && data.login}</h2>
+
+          <p className="full1">{data && data.login}</p>
+          <br />
+          <button>Follow </button>
+          {/* <p>{data.followers} followers {data.following} following</p> */}
+          <p className="full">Full stack Web Developer</p>
+          <br />
+          <p>{data && data.campany}</p>
+          <p>{data && data.location}</p>
+          <p>{data && data.email}</p>
+          <p>{ data && data.id}</p>
+          <p>{data && data.twitter_username}</p>
+        </div>
     </div>
+   </div>
+    
   );
 }
+
 
 export default App;
